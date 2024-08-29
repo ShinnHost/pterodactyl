ead2f92494d5888663c86539ea7a42d9f1906087
ShinnHost <arxzofficial@gmail.com>
2024-08-29 20:00:59 +0700

    Add files via upload

---
+++ installpanelv2.sh
@@ -0,0 +1,29 @@
+
+
+# Minta input dari pengguna
+read -p "Masukkan domain: " domain
+read -p "Masukkan Email: " email
+read -p "Masukkan Password Login: " password
+read -p "Masukkan Subdomain Panel: " subdomain
+read -p "Masukkan Domainnode: " domainnode
+
+bash <(curl -s https://pterodactyl-installer.se) <<EOF
+0
+username
+user7726
+1
+Asia/Jakarta
+$email
+$email
+admin
+adm
+adm
+$password
+$subdomain
+y
+y
+y
+y
+yes
+A
+EOF
